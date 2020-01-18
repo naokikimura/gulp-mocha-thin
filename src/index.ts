d6885c20cda25a50b6f316b98da0a0c3a2c3a3a8
Naoki Kimura <n.kimura.cap@gmail.com>
2020-01-18 23:24:27 +0900

    Fix a bug

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import Mocha from 'mocha';
 import MochaOptions from 'mocha/lib/cli/options';
 import stream from 'stream';
+import util from 'util';
+
+const debug = util.debuglog('gulp-mocha-thin');
 
 function isMochaOptions(options: string | string[] | Mocha.MochaOptions): options is Mocha.MochaOptions {
-  return !(options instanceof String || Array.isArray(options));
+  return !(typeof options === 'string' || Array.isArray(options));
 }
 
 function requireModule(modules: string | string[]): void {
   (Array.isArray(modules) ? modules : [modules]).filter(id => id).forEach(require);
 }
 
-export = (options: string | string[] | Mocha.MochaOptions = {}) => {
+export = (options: string | string[] | Mocha.MochaOptions = '') => {
   const mochaOptions = isMochaOptions(options) ? options : MochaOptions.loadOptions(options);
+  debug(`mochaOptions = ${util.inspect(mochaOptions)}`);
   requireModule(mochaOptions && (mochaOptions as any).require);
   const mocha = new Mocha(mochaOptions);
   return new stream.Transform({
